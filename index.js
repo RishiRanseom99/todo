e4b5f1ca11f3b3132703eeb9094bcfe35aca15da
RishiRanseom99 <41407371+RishiRanseom99@users.noreply.github.com>
2020-05-23 22:37:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-
-
+var firebaseConfig = {
+    apiKey: "AIzaSyDZuPjJhHmugQY-6kLrZ-F6NJ26Z3vCE4g",
+    authDomain: "task-list-d4dd3.firebaseapp.com",
+    databaseURL: "https://task-list-d4dd3.firebaseio.com",
+    projectId: "task-list-d4dd3",
+    storageBucket: "task-list-d4dd3.appspot.com",
+    messagingSenderId: "703233572908",
+    appId: "1:703233572908:web:6739d6c4c488dc86f85189",
+    measurementId: "G-PFH2MW6QV3"
+  };
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+var messagesRef=firebase.database().ref('messages');
 document.getElementById('form').addEventListener('submit',submitform);
 
 function submitform(e){
@@ -8,7 +19,7 @@ function submitform(e){
     var d=getinputValue('deadline');
     console.log(d);
     
-    //saveMessage(t,d);
+    saveMessage(t,d);
     
 }
 
